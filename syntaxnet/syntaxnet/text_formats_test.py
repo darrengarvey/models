defd026d3febdc735e3730318b8198fe7b7c1aed
David Mansfield <dmansfield@gmail.com>
2016-06-29 17:38:23 +0000

    Fix two tests that use document_source

--- syntaxnet/syntaxnet/text_formats_test.py
+++ syntaxnet/syntaxnet/text_formats_test.py
@@ -79,8 +79,9 @@ class TextFormatsTest(test_util.TensorFlowTestCase):
     logging.info('Writing text file to: %s', self.corpus_file)
     with open(self.corpus_file, 'w') as f:
       f.write(sentence)
+    unused_text_input = tf.constant([], tf.string)
     sentence, _ = gen_parser_ops.document_source(
-        task_context=self.context_file, batch_size=1)
+        text=unused_text_input, task_context=self.context_file, batch_size=1)
     with self.test_session() as sess:
       sentence_doc = self.ReadNextDocument(sess, sentence)
       self.assertEqual(' '.join([t.word
